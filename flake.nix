f10bb5daf9e145961fc4d9b0f67bf301523ddd97
pnotequalnp <kevin@pnotequalnp.com>
2026-03-06 11:00:48 -0800

    Improve flake
    Changed to track the Nixpkgs branch instead of NixOS unstable, and added
    a package output

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
   description = "";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, ... }: 
-  {
-    overlay = final: prev: { tmodloader-server = final.callPackage ./pkgs/default.nix {}; };
-    overlays.default = self.overlay;
-    nixosModules.tmodloader = import ./module;
-  };
+  outputs =
+    { self, nixpkgs }:
+    {
+      overlay = final: prev: { tmodloader-server = final.callPackage ./pkgs/default.nix { }; };
+      overlays.default = self.overlay;
+      nixosModules.tmodloader = import ./module;
+      packages.x86_64-linux.default = (
+        import nixpkgs {
+          system = "x86_64-linux";
+          overlays = [ self.overlays.default ];
+        }
+      ).tmodloader-server;
+    };
 }
